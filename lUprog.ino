aedf740c0917a1063c129642982a8c8077b28093
Luproger <lukas0611@yandex.ru>
2025-04-27 23:37:40 +0300

    Add test menu

--- lUprog.ino
+++ lUprog.ino
@@ -10,10 +10,26 @@
 
 #define SETTINGS_COUNT 5
 
+enum {
+  MAIN_SCREEN,
+  SETTINGS,
+  SET_MK,
+  BOOT_TO_CD,
+  FUSE,
+  FLASH,
+  EEPROM,
+  BOOTLOADER,
+  READ,
+  WRITE,
+  ARD_BOOT,
+  STD_BOOT,
+}Menu;
+
+Menu = MAIN_SCREEN;
 typedef struct {
   int spi_speed;
   int sleep_delay;
-}params_t;
+} params_t;
 
 
 
@@ -24,61 +40,80 @@ typedef struct  {
   int *value;
   int min;
   int max;
-}setting_t;
+} setting_t;
 
 
-setting_t settings[SETTINGS_COUNT]={
-  {"SPI speed",&params.spi_speed,1,10},
-  {"SleepDelay",&params.sleep_delay,1,100},
+setting_t settings[SETTINGS_COUNT] = {
+  {"SPI speed", &params.spi_speed, 1, 10},
+  {"SleepDelay", &params.sleep_delay, 1, 100},
 };
 
 GyverOLED<SSD1306_128x32, OLED_NO_BUFFER> oled;
 GButton lBtn(BTN_LEFT);
 GButton OkBtn(BTN_OK);
-GButton rBtn(POWER_KEY,LOW_PULL);
-void Settings(){
+GButton rBtn(POWER_KEY, LOW_PULL);
+void Settings() {
   static uint8_t ptr;
   drawMenu(ptr);
 }
-void drawMenu(uint8_t& page ){
+void drawMenu(uint8_t& page ) {
   //oled.clear();
   oled.setScale(2);
-  oled.setCursor(10,2);
+  oled.setCursor(10, 2);
   oled.print(settings[page].pageName);
   oled.setCursor(10, 3);
-  oled.fastLineH(3,10,100);
+  oled.fastLineH(3, 10, 100);
 }
-void btnTick(){
+void btnTick() {
   rBtn.tick();
   OkBtn.tick();
   lBtn.tick();
 }
 void setup() {
-  pinMode(A2,INPUT);
+  pinMode(A2, INPUT);
   //digitalWrite(PODXVAT,HIGH);
   Serial.begin(9600);
-  oled.init();        
+  oled.init();
   oled.clear();
-         
+
 }
 
 void loop() {
+  
   btnTick();
   //Settings();
-  if(lBtn.isClick()){
-    oled.clear();
-    oled.setCursor(0,0);
-    oled.print("lBTN");
-    }
-    if(rBtn.isClick()){
-    oled.clear();
-    oled.setCursor(0,0);
-    oled.print("rBTN");
-    }
-    if(OkBtn.isClick()){
-    oled.clear();
-    oled.setCursor(0,0);
-    oled.print("OkBTN");
-    }
-    
+  switch (Menu) {
+    case MAIN_SCREEN:
+
+      break;
+    case SETTINGS:
+
+      break;
+    case SET_MK:
+
+      break;
+    case BOOT_TO_CD:
+      break;
+    case FUSE:
+
+      break;
+    case EEPROM:
+
+      break;
+    case BOOTLOADER:
+
+      break;
+    case READ:
+
+      break;
+    case WRITE:
+
+      break;
+    case ARD_BOOT:
+
+      break;
+    case STD_BOOT:
+
+      break;
+  }
 }
