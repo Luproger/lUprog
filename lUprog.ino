d9596f89b25d6e7f8b01395a2c25560c8c0e855a
Luproger <lukas0611@yandex.ru>
2025-05-06 15:19:37 +0300

    Реализовано основное меню

--- lUprog.ino
+++ lUprog.ino
@@ -10,22 +10,39 @@
 
 #define SETTINGS_COUNT 5
 
-enum {
+typedef enum {
   MAIN_SCREEN,
   SETTINGS,
   SET_MK,
   BOOT_TO_CD,
   FUSE,
   FLASH,
-  EEPROM,
+  EEPROM_SETUP,
   BOOTLOADER,
   READ,
   WRITE,
   ARD_BOOT,
   STD_BOOT,
-}Menu;
+} menu_t;
 
-Menu = MAIN_SCREEN;
+char *stateName[] = {
+  "lUprog",
+  "SETTINGS",
+  "SET_MK",
+  "BOOT_TO_CD",
+  "FUSE",
+  "FLASH",
+  "EEPROM",
+  "BOOTLOADER",
+  "READ",
+  "WRITE",
+  "ARD_BOOT",
+  "STD_BOOT",
+};
+
+
+menu_t Menu = MAIN_SCREEN;
+
 typedef struct {
   int spi_speed;
   int sleep_delay;
@@ -52,18 +69,19 @@ GyverOLED<SSD1306_128x32, OLED_NO_BUFFER> oled;
 GButton lBtn(BTN_LEFT);
 GButton OkBtn(BTN_OK);
 GButton rBtn(POWER_KEY, LOW_PULL);
+
 void Settings() {
   static uint8_t ptr;
-  drawMenu(ptr);
+  //drawMenu(ptr);
 }
-void drawMenu(uint8_t& page ) {
+/*void drawMenu(uint8_t& page ) {
   //oled.clear();
   oled.setScale(2);
   oled.setCursor(10, 2);
   oled.print(settings[page].pageName);
   oled.setCursor(10, 3);
   oled.fastLineH(3, 10, 100);
-}
+  }*/
 void btnTick() {
   rBtn.tick();
   OkBtn.tick();
@@ -78,42 +96,174 @@ void setup() {
 
 }
 
+bool isFirst = true;
+
+void changeSt(menu_t newSt, int line) {
+  Serial.print("Переход в новое состояние ");
+  Serial.print(stateName[newSt]);
+  Serial.print(", на строке ");
+  Serial.println(line);
+  isFirst = true;
+  Menu = newSt;
+}
+void drawMenuPtr() {
+  static uint16_t blinkTimer;
+  static bool blinkF;
+  if (millis() - blinkTimer >= 500) {
+    if (blinkF) oled.clear(0, 29, 132, 31);
+    else oled.fastLineH(30, 0, 132);
+    blinkF = !blinkF;
+    blinkTimer = millis();
+  }
+}
+
+void drawMenu() {
+  if (isFirst) {
+    oled.clear();
+    oled.setScale(2);
+    oled.setCursor(0, 1);
+    oled.print(stateName[Menu]);
+    isFirst = false;
+
+  }
+}
+
 void loop() {
-  
   btnTick();
-  //Settings();
+  drawMenu();
+  drawMenuPtr();
   switch (Menu) {
     case MAIN_SCREEN:
-
+      if (OkBtn.isClick()) {
+        changeSt(SETTINGS, __LINE__);
+      }
       break;
     case SETTINGS:
-
+      if (OkBtn.isHold()) {
+        changeSt(MAIN_SCREEN, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(SETTINGS, __LINE__);
+      }
+      if (rBtn.isClick()) {
+        changeSt( SET_MK, __LINE__);
+      }
       break;
     case SET_MK:
-
+      if (lBtn.isClick()) {
+        changeSt(SETTINGS, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(BOOT_TO_CD, __LINE__);
+      }
       break;
     case BOOT_TO_CD:
+      if (OkBtn.isHold()) {
+        changeSt(SET_MK, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(BOOT_TO_CD, __LINE__);
+      }
+      if (rBtn.isClick()) {
+        changeSt( FUSE, __LINE__);
+      }
       break;
     case FUSE:
-
+      if (OkBtn.isHold()) {
+        changeSt(SET_MK, __LINE__);
+      }
+      if (lBtn.isClick()) {
+        changeSt(BOOT_TO_CD, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(READ, __LINE__);
+      }
+      if (rBtn.isClick()) {
+        changeSt( FLASH, __LINE__);
+      }
       break;
-    case EEPROM:
-
+    case FLASH:
+      if (OkBtn.isHold()) {
+        changeSt(SET_MK, __LINE__);
+      }
+      if (lBtn.isClick()) {
+        changeSt(FUSE, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(READ, __LINE__);
+      }
+      if (rBtn.isClick()) {
+        changeSt( EEPROM_SETUP, __LINE__);
+      }
+      break;
+    case EEPROM_SETUP:
+      if (OkBtn.isHold()) {
+        changeSt(SET_MK, __LINE__);
+      }
+      if (lBtn.isClick()) {
+        changeSt(FLASH, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(READ, __LINE__);
+      }
+      if (rBtn.isClick()) {
+        changeSt( BOOTLOADER, __LINE__);
+      }
       break;
     case BOOTLOADER:
-
+      if (OkBtn.isHold()) {
+        changeSt(SET_MK, __LINE__);
+      }
+      if (lBtn.isClick()) {
+        changeSt(EEPROM_SETUP, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(ARD_BOOT, __LINE__);
+      }
       break;
     case READ:
-
+      if (OkBtn.isHold()) {
+        changeSt(BOOT_TO_CD, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(MAIN_SCREEN, __LINE__);
+      }
+      if (rBtn.isClick()) {
+        changeSt( WRITE, __LINE__);
+      }
       break;
     case WRITE:
-
+      if (OkBtn.isHold()) {
+        changeSt(BOOT_TO_CD, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(MAIN_SCREEN, __LINE__);
+      }
+      if (lBtn.isClick()) {
+        changeSt( READ, __LINE__);
+      }
       break;
     case ARD_BOOT:
-
+      if (OkBtn.isHold()) {
+        changeSt(BOOTLOADER, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(MAIN_SCREEN, __LINE__);
+      }
+      if (rBtn.isClick()) {
+        changeSt( STD_BOOT, __LINE__);
+      }
       break;
     case STD_BOOT:
-
+      if (OkBtn.isHold()) {
+        changeSt(BOOTLOADER, __LINE__);
+      }
+      if (OkBtn.isClick()) {
+        changeSt(MAIN_SCREEN, __LINE__);
+      }
+      if (lBtn.isClick()) {
+        changeSt( ARD_BOOT, __LINE__);
+      }
       break;
   }
 }
